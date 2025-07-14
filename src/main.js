d516f2f01a344d5db1065657620e8e1ad2018b79
tumelo-sebego <145678194+tumelo-sebego@users.noreply.github.com>
2025-07-14 22:10:21 +0000

    modified main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import './style.css'
+import router from './router'
+import store from './store'
+import './assets/css/main.css' // Import your Tailwind CSS
 
-createApp(App).mount('#app')
+createApp(App).use(store).use(router).mount('#app')
